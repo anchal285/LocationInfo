2c65a3599d721fb17e2a305a3330a7e8d7575094
Anchal <anchal2805gupta@gmail.com>
2023-11-15 11:28:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+
+// App.js
+import React, { useState, useEffect } from 'react';
+import UserInput from './Input';
+import LocationInfo from './Locationdetails';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchLocationInfo = async (postalCode) => {
+    setLoading(true);
+    setError(null);
+
+    const apiUrl = `http://api.zippopotam.us/IN/${postalCode}`; 
+
+    try {
+      const response = await axios.get(apiUrl);
+      setLocation(response.data);
+      console.log('Data fetched successfully');
+    } catch (error) {
+      setError('Failed to fetch location information. Please try again.');
+      console.error('API request failed:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const clearLocationInfo = () => {
+    // Clear location and error information
+    setLocation(null);
+    setError(null);
+  };
+
+  return (
+    <div className="container mx-auto p-8 bg-blue-200 main-background">
+      
+      <UserInput onSearch={fetchLocationInfo} loading={loading} />
+      <LocationInfo location={location} error={error} onClear={clearLocationInfo} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Locationdetails.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+function LocationInfo({ location, error, onClear }) {
+  return (
+    <div className="p-4 text-center back">
+      {error ? (
+        <div className="text-red-600">{error}</div>
+      ) : location ? (
+        <div>
+          <h2 className="text-2xl font-bold title">Location Information</h2>
+          <p className="mt-4 count">Country: {location.country}</p>
+          {location.places.map((place, index) => (
+            <div key={index} className="mt-4 extra">
+              <p>State: {place.state}</p>
+              <p>Place Name: {place['place name']}</p>
+            </div>
+          ))}
+            
+          <button
+            onClick={onClear}
+            className="mt-4 px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-650"
+          >
+            Clear
+          </button>
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+export default LocationInfo;
